4c5294f9ead9fe2b611d233baa314d6b94797b64
meir_p <meir12345p@gmail.com>
2025-11-02 15:00:03 +0200

    final commit

--- Report.py
+++ Report.py
@@ -1,11 +1,6 @@
-
-
 class Report:
 
-    def __init__(self,summary,urgency_level,submitted_by):
+    def __init__(self, summary, urgency_level, submitted_by):
         self.summary = summary
         self.urgency_level = urgency_level
         self.submitted_by = submitted_by
-
-
-
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from Agent_class import Agent
 from Mission_class import Mission
 from Report import Report
 from MissionControl import MissionControl
+from Class_IntelTools import IntelTools
+
 
 mission = Mission("to run","Netanya")
 mission.assign(Agent("Meir", 3))
@@ -13,4 +15,5 @@ agent = Agent("chaim",5)
 
 re = Report("sudan",5,agent.code_name)
 MissionControl.analyze_report(re)
+print(IntelTools.log_transmission(agent.code_name,MissionControl.analyze_report(re)))
 
